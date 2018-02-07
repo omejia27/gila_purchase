3e01e3e7083d4bff4da5286de71b8be4da5516dc
Omar Mejia <omejia@gila.com.mx>
2018-02-07 15:03:10 -0700

    [ADD] gila_purchase: Onchange project in purchase order to have default warehouse from project warehouse

--- models/purchase.py
+++ models/purchase.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class PurchaseOrder(models.Model):
@@ -9,3 +9,12 @@ class PurchaseOrder(models.Model):
     project_id = fields.Many2one(
         'project.project',
         string='Project')
+
+    @api.onchange('project_id')
+    def _onchange_project_id(self):
+        stock_picking_type = self.env['stock.picking.type']
+        domain = ['|', ('name', 'ilike', self.project_id.name),
+                  ('name', 'ilike', self.project_id.code),
+                  ('code', '=', 'incoming')]
+        picking_type_id = stock_picking_type.search(domain)
+        self.picking_type_id = picking_type_id.id
