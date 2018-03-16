5f6ace13107c46f622e867faa05b2eb74c0b6447
Omar Mejia Villavicencio <omejia@gila.com.mx>
2018-03-15 20:20:21 -0400

    [ADD] gila_purchase: adding new to change default colors from purchase request lines

--- __openerp__.py
+++ __openerp__.py
@@ -11,11 +11,12 @@
     'category': 'Purchase',
     'version': '9.0.0.1',
 
-    'depends': ['base','purchase','project'],
+    'depends': ['base','purchase','project','task_resource'],
 
     'data': [
         # 'security/ir.model.access.csv',
         'views/purchase_order_view.xml',
         'views/product_template_view.xml',
+        'views/purchase_request_view_inherit.xml',
     ],
 }
